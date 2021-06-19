66e75c350ad20db1fe160a8582a69a7c8110101e
Hussyvel <hussyribeiro@hotmail.com>
2021-06-19 18:45:16 -0300

    programas com For, While e Enhanced-For

--- Exercicios/src/fundamentos/Testes.java
+++ Exercicios/src/fundamentos/Testes.java
@@ -1,9 +1,16 @@
 package fundamentos;
 
 
+import java.util.Scanner;
+
 public class Testes {
     public static void main(String[] args) {
 
+        int a = 2;
+        do {
+            System.out.println("Imprima esta linha");
+            a++;
+        }while (a < 10);
 
     }
 }
---
+++ Exercicios/src/fundamentos/lacoFor.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class lacoFor {
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x++) {
+            if (x > 5 && x < 10){
+                System.out.println(x);
+            }
+        }
+    }
+}
