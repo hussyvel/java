1eaa7809e0c2ded1d80477dfa95d3a5311743ee2
hussyvel <hussyribeiro@hotmail.com>
2024-09-09 11:43:15 -0300

    update do projeto de testes

---
+++ apenas_testes/src/main/java/br/com/hussyvel/java/Main.java
@@ -0,0 +1,15 @@
+package br.com.hussyvel.java;
+
+public class Main {
+    public Main(String name, double price) {
+
+    }
+
+    public static void main(String[] args) {
+
+        private String name;
+        private double price;
+
+
+    }
+}
