96c2755d48725044e5abb6daf9eb158c6b748c49
hussyvel <hussyribeiro@hotmail.com>
2024-10-25 16:43:29 -0300

    update

--- java8ComoProgramar/Program.java
+++ java8ComoProgramar/Program.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Program {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double m;
+        double a;
+        double b;
+
+        System.out.printf("Digite o primeiro valor: ");
+        a = input.nextDouble();
+
+        System.out.printf("Digite o segundo valor: ");
+        b = input.nextDouble();
+
+        m = (a + b) / 5.0;
+
+        System.out.printf("A soma de %.2f e %.2f, dividido por 5.0 é: %.2f\n", a, b, m);
+
+        input.close();
+    }
 }
