196fe90000c661ade4185f48f37e41601dcc3710
hussyvel <hussyribeiro@hotmail.com>
2024-11-28 22:20:07 -0300

    update

--- AcimaDiagonalEspecializada/src/application/Main.java
+++ AcimaDiagonalEspecializada/src/application/Main.java
@@ -1,7 +1,36 @@
 package application;
 
+import entities.MatrixProcessor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Informe o tamanho da matriz quadrada? :");
+            int size = sc.nextInt();
+
+            if (size <= 0) {
+                System.out.println("O tamanho deve ser maior qu zero.");
+                return;
+            }
+
+            MatrixProcessor matrixProcessor = new MatrixProcessor(size);
+
+            matrixProcessor.fillMatrix(sc);
+
+            int sum = matrixProcessor.calculandoASomaDaDiagonal();
+            System.out.println("Soma dos elementos acima da diagonal principal " + sum);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            sc.close();
+        }
     }
 }
--- AcimaDiagonalEspecializada/src/entities/MatrixProcessor.java
+++ AcimaDiagonalEspecializada/src/entities/MatrixProcessor.java
@@ -14,7 +14,7 @@ public class MatrixProcessor {
     public void fillMatrix(Scanner sc){
         System.out.println("Preenchendo a matriz:");
         for (int i = 0; i < size; i++){
-            for (int j =0; j < size; i++){
+            for (int j =0; j < size; j++){
                 System.out.printf("Elemento [%d, %d]: ", i, j);
                 matriz[i][j] = sc.nextInt();
             }
