2acd4e97ab454495760c820584bf5ca4040b22bd
hussyvel <hussyribeiro@hotmail.com>
2024-12-02 15:41:06 -0300

    .

--- AprovadosEspecializado/src/application/Main.java
+++ AprovadosEspecializado/src/application/Main.java
@@ -1,7 +1,38 @@
 package application;
 
+import entities.AprovadosEspecializados;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Apenas contando quantas vezes: ");
+        int n = sc.nextInt();
+
+        //Array de objetos
+        AprovadosEspecializados[] aprovadosEspecializados = new AprovadosEspecializados[n];
+
+        for (int i = 0; i < n; i++){
+            System.out.println("Digite o nome, a primeira e segunda nota:");
+            sc.nextLine();
+            String name = sc.nextLine();
+            double nota1 = sc.nextDouble();
+            double nota2 = sc.nextDouble();
+
+            aprovadosEspecializados[i] = new AprovadosEspecializados(name, nota1, nota2);
+        }
+
+        System.out.println("Alunos aprovados: ");
+        for (AprovadosEspecializados aprovadosEspecializados1 : aprovadosEspecializados) {
+            if (aprovadosEspecializados1.estaAprovado()){
+                System.out.println(aprovadosEspecializados1.getName());
+            }
+        }
+
+        sc.close();
     }
 }
