88d5ed0ebffdef6710c7bb30cc56ce778623382e
hussyvel <hussyribeiro@hotmail.com>
2024-12-22 17:26:28 -0300

    Exercicio de listas em java

---
+++ Exercicio_Listas/src/application/Main.java
@@ -0,0 +1,67 @@
+package application;
+
+import entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("How many employees will be registered? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            System.out.println("Employee #" + (i + 1) + ":");
+            System.out.print("Id: ");
+            Integer id = sc.nextInt();
+
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.print("Salary: ");
+            Double salary = sc.nextDouble();
+
+            Employee emp = new Employee(id, name, salary);
+
+            list.add(emp);
+        }
+
+        System.out.println();
+        System.out.print("Entre com o id do funcionário: ");
+        int idSalary = sc.nextInt();
+
+        Integer pos = positionId(list, idSalary);
+        if (pos == null){
+            System.out.println("Posição não encontrada!!!");
+        }else {
+            System.out.println("Entre com a porcentagem a ser adicionada: ");
+            double percent = sc.nextDouble();
+            list.get(pos).increaseSalary(percent);
+
+            for (Employee emp : list){
+                System.out.println(emp);
+            }
+        }
+
+        sc.close();
+    }
+
+    public static Integer positionId(List<Employee> list, int id) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
