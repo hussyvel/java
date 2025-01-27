ed829869ac2d7d0287244102998c5abbc1b354ff
hussyvel <hussyribeiro@hotmail.com>
2025-01-26 22:53:55 -0300

    update de listas

--- ExercicioProposto/src/application/Main.java
+++ ExercicioProposto/src/application/Main.java
@@ -1,7 +1,78 @@
 package application;
 
+import entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.println("Quantos empregados vc deseja registrar? ");
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("Employee # " + (i + 1) + " :");
+            System.out.println("ID: ");
+            Integer id = sc.nextInt();
+            while (hasId(list, id)) {
+                System.out.println("O id não existe, tente novamente: ");
+                id = sc.nextInt();
+            }
+
+            System.out.println("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.println("Salary: ");
+            Double salary = sc.nextDouble();
+
+            Employee emp = new Employee(id, name, salary);
+
+            list.add(emp);
+        }
+
+        System.out.println();
+        System.out.println("Entre com o id do funcionário que terá o salário incrementado: ");
+        int idSalary = sc.nextInt();
+
+        Employee emp = list.stream().filter(x -> x.getId() == idSalary).findFirst().orElse(null);
+
+        if (emp == null){
+            System.out.println("Entre com outro valor id");
+        }else {
+            System.out.println("Entre com o valor da porcentagem a ser adicionada: ");
+            double percent = sc.nextDouble();
+            emp.increment(percent);
+        }
+
+        System.out.println();
+        System.out.println("Listar funcionários: ");
+        for (Employee e : list){
+            System.out.println(e);
+        }
+
+        sc.close();
+    }
+
+    public static Integer posicao (List<Employee> list, int id){
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).getId()==id){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public static boolean hasId(List<Employee> list, int id){
+        Employee emp = list.stream().filter(x ->x.getId() == id).findFirst().orElse(null);
+        return emp != null;
     }
 }
--- ExercicioProposto/src/entities/Employee.java
+++ ExercicioProposto/src/entities/Employee.java
@@ -3,5 +3,50 @@ package entities;
 public class Employee {
 
     private Integer id;
-    private 
+    private String name;
+    private Double salary;
+
+    public Employee() {
+
+    }
+
+    public Employee(Integer id, String name, Double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+    public Double getSalary(){
+        return salary;
+    }
+
+    public void setSalary(){
+        this.salary = salary;
+    }
+
+    public void increment(double percent){
+        salary += salary * percent / 100;
+    }
+
+    @Override
+    public String toString(){
+        return id + ", " + name + ", " + String.format("%.2f", salary);
+    }
+
 }
